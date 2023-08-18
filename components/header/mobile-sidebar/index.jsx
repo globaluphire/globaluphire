f1dca802dd27082ab6fbb46d2a218385a5aefbdb
Mounish Soni <mounishsoni116@gmail.com>
2023-08-18 17:32:17 -0400

    added facility selection in Mobile Version

--- components/header/mobile-sidebar/index.jsx
+++ components/header/mobile-sidebar/index.jsx
@@ -20,13 +20,68 @@ import { useDispatch, useSelector } from "react-redux";
 import employerMenuData from "../../../data/employerMenuData";
 import candidatesMenuData from "../../../data/candidatesMenuData";
 import { logout } from "../../../utils/logout";
+import { Typeahead } from "react-bootstrap-typeahead";
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import { setFacility } from "../../../features/employer/employerSlice";
+import { Tooltip } from 'react-tooltip'
+import { useEffect, useState } from "react";
+import { supabase } from "../../../config/supabaseClient";
+
 
 const Index = () => {
+  // global states
+  const facility = useSelector(state => state.employer.facility.payload)
+
+  // set initial facility value
+  let localStorageFacilityArray = []
+  if (facility) {
+    localStorageFacilityArray.push(facility)
+  }
+
+  const [facilityNames, setFacilityNames] = useState([]);
+  const [facilitySingleSelections, setFacilitySingleSelections] = useState(localStorageFacilityArray);
+
   const router = useRouter();
   const dispatch = useDispatch();
 
   const user = useSelector(state => state.candidate.user)
   const menuOptions = user.role !== 'CANDIDATE' ?  employerMenuData : candidatesMenuData
+
+  async function getFacilityNames() {
+    // call reference to get applicantStatus options
+    let { data: refData, error: e } = await supabase
+    .from('reference')
+    .select("*")
+    .eq('ref_nm',  'facilityName');
+
+    if (refData) {
+        // setFacilityNames(refData)
+        let facilities = []
+        for (let i = 0; i < refData.length; i++) {
+          facilities.push(refData[i].ref_dspl)
+        }
+        facilities.sort()
+        setFacilityNames(facilities)
+    }
+  }
+
+  useEffect(() => {
+    getFacilityNames();
+    if(facility) {
+      let facilityArray = []
+      facilityArray.push(facility)
+      setFacilitySingleSelections(facilityArray)
+    }
+  }, []);
+
+  useEffect(() => {
+    if (facilitySingleSelections) {
+      dispatch(setFacility(facilitySingleSelections[0]))
+    } else {
+      dispatch(setFacility(''))
+    }
+  }, [facilitySingleSelections])
 
   return (
     <div
@@ -39,6 +94,37 @@ const Index = () => {
       {/* End pro-header */}
 
       <ProSidebarProvider>
+
+          <div className="outer-box">
+          {/* <!-- Dashboard Option --> */}
+          { user.role !== 'CANDIDATE' ?
+            <Form>
+              <Col>
+                <Form.Group>
+                    <Typeahead
+                        id="facilityNames"
+                        onChange={setFacilitySingleSelections}
+                        className="chosen-single form-input chosen-container"
+                        placeholder="Select Facility"
+                        options={facilityNames}
+                        selected={facilitySingleSelections}
+                        style={{ minWidth: '300px' }}
+                        required
+                    />
+                </Form.Group>
+
+              </Col>
+            </Form>
+            : '' }
+          {/* End dropdown */}
+          </div>
+            { user.role !== 'CANDIDATE' ?
+              <div className="option-box">
+                <a data-tooltip-id="facility-tooltip" data-tooltip-content="This is applicable to all admin pages, if you want to show data for all facilities then don't select any!">
+                  <span className="lar la-question-circle" style={{ fontSize: '24px', margin: '5px 10px' }}></span>
+                </a>
+                <Tooltip id="facility-tooltip" />
+              </div> : '' }
         {/* <Sidebar> */}
         {user ? 
           <Menu>
