23b0c4773f6e4eb3fdeca718712875d9afa96994
asambhavDeveloper <developer@asambav.in>
2023-09-13 21:59:07 +0530

    more info about user

--- components/dashboard-pages/employers-dashboard/user-documents/components/UserDetails.jsx
+++ components/dashboard-pages/employers-dashboard/user-documents/components/UserDetails.jsx
@@ -1,9 +1,26 @@
 const PostJobSteps = ({applicantData}) => {
+  if(!applicantData) return null;
+
   return (
     <div className="post-job-steps">
       <div className="step">
         <span className="icon flaticon-user"></span>
+        <div>
+
         <h5>{applicantData?.name}</h5>
+        <small>
+          <a  
+            href={JSON.parse(applicantData?.doc_dwnld_url)?.publicUrl}
+            style={{
+              display: "flex",
+              gap: "0.5rem",
+            }}
+          >
+            {applicantData?.doc_name}
+          </a>
+        </small>
+        </div>
+
       </div>
 
       <div className="step">
@@ -13,7 +30,20 @@ const PostJobSteps = ({applicantData}) => {
 
       <div className="step">
         <span className="icon flaticon-map-locator"></span>
-        <h5>{applicantData?.facility_name}</h5>
+        <div>
+          <h5>{applicantData?.facility_name}</h5>
+          <small>{applicantData?.job_comp_add}</small>
+        </div>
+      </div>
+      
+      <div className="step">
+        <span className="icon flaticon-tick"></span>
+        <div>
+          <h5>Status</h5>
+          <small>{applicantData?.status}</small>
+          {applicantData?.status.toLowerCase() === "hired" && <small> @ {new Date(applicantData?.hired_date).toLocaleDateString()}</small>}
+          {applicantData?.status.toLowerCase() === "reject" && <small> @ {new Date(applicantData?.rejected_date).toLocaleDateString()}</small>}
+        </div>
       </div>
     </div>
   );
