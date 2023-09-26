2bda32d56c63f069500bce8b8fd0ebd3cb95f0ae
Mounish Soni <mounishsoni116@gmail.com>
2023-09-26 12:59:15 -0400

    minor fix

--- components/dashboard-pages/employers-dashboard/manage-jobs/components/JobListingsTable.jsx
+++ components/dashboard-pages/employers-dashboard/manage-jobs/components/JobListingsTable.jsx
@@ -79,7 +79,7 @@ const JobListingsTable = () => {
     //   setEmailError("Please enter a valid email address");
     //   isValid = false;
     // }
-    if (phoneNumber && phoneNumber.length < 12) {
+    if (phoneNumber && phoneNumber.length > 2 && phoneNumber.length < 12) {
       setPhoneNumberError("Phone number should be in +11234567890");
       isValid = false;
     } else {
