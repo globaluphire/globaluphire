8c5e1694b18b81235d7ce83bf55a16e59222ab92
globaluphire <133300609+globaluphire@users.noreply.github.com>
2023-10-02 12:02:34 -0400

    Update AppSection.jsx

--- components/app-section/AppSection.jsx
+++ components/app-section/AppSection.jsx
@@ -3,7 +3,7 @@ const AppSection = () => {
     <div className="row">
       <div className="image-column col-lg-6 col-md-12 col-sm-12">
         <div className="bg-shape"></div>
-        <figure className="image" data-aos="fade-right">
+        <figure className="image" data-aos="fade-right"> 
           <img src="images/resource/mobile-app.png" alt="mobile app" />
         </figure>
       </div>
