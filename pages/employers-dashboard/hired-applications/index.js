950a59ccb4855231911a44f5bfd4a737c0fc8eb7
asambhavDeveloper <developer@asambhav.in>
2023-10-18 17:18:19 +0530

    fix : candidates access to hired-applicants page denied

--- pages/employers-dashboard/hired-applications/index.js
+++ pages/employers-dashboard/hired-applications/index.js
@@ -6,22 +6,29 @@ import { useEffect } from "react";
 import HiredApplications from "../../../components/dashboard-pages/employers-dashboard/hired-applications";
 
 const index = () => {
+  const user = useSelector((state) => state.candidate.user);
+  const isEmployer = ["SUPER_ADMIN", "ADMIN", "MEMBER"].includes(user.role);
 
-    const user = useSelector(state => state.candidate.user)
-    const isEmployer = ['SUPER_ADMIN', 'ADMIN', 'MEMBER'].includes(user.role)
+  useEffect(() => {
+    if (!isEmployer) {
+      Router.push("/");
+    }
+  }, []);
 
-    useEffect(() => {
-      if (!isEmployer) {
-        Router.push("/")
-      }
-    }, []);
-
-    return (
-      <> 
-        <Seo pageTitle="Hired Applicants" />
-        <HiredApplications />
-      </>
-    );
+  return (
+    <>
+      {" "}
+      {isEmployer ? (
+        <>
+          {" "}
+          <Seo pageTitle="Hired Applicants" />
+          <HiredApplications />
+        </>
+      ) : (
+        ""
+      )}
+    </>
+  );
 };
 
 export default dynamic(() => Promise.resolve(index), { ssr: false });
