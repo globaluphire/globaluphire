d14aa369a31d857babe652d54492b8d1979fe542
asambhavDeveloper <developer@asambhav.in>
2023-10-25 19:10:55 +0530

    fix : config import fix

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -1,17 +1,17 @@
 import { Twilio } from "twilio";
-import { config } from "../../config/env";
+import { envConfig } from "../../config/env";
 
 export default async function handler(req, res) {
     const twilioClient = new Twilio(
-        config.TWILIO_ACCOUNT_SID,
-        config.TWILIO_AUTH_TOKEN
+        envConfig.TWILIO_ACCOUNT_SID,
+        envConfig.TWILIO_AUTH_TOKEN
     );
     if (req.method === "POST") {
         try {
             const response = await twilioClient.messages.create({
                 body: req.body.content,
                 // Check number from Twilio if changed
-                from: config.TWILIO_NUMBER,
+                from: envConfig.TWILIO_NUMBER,
                 to: req.body.recipient,
             });
             return res.status(200).json({ status: "success", data: response });
