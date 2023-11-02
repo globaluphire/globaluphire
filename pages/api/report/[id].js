2ae6016701eac40ab679979a2ec0b746c81978ae
asambhavDeveloper <developer@asambhav.in>
2023-11-02 16:56:52 +0530

    feat : error handling and using rawUnsafe to query using prisma

--- pages/api/report/[id].js
+++ pages/api/report/[id].js
@@ -1,19 +1,26 @@
-// import { supabase } from "../../../config/supabaseClient";
 import { PrismaClient } from "@prisma/client";
 import { reportItems } from "../../../data/reports";
 
 export default async function handler(req, res) {
     try {
         const qry = new PrismaClient();
-        const itemId = (parseInt(req.query.id) + 1).toString();
-        const report = reportItems.filter((item) => item.reportId === itemId);
-        // //console.log(report);
-        // const result = await qry.$queryRaw`SELECT COUNT(*) FROM APPLICATIONS`;
-        // console.log(report[0].query);
-        const result = await qry.$queryRaw`${report[0].query}`;
+        const id = parseInt(req.query.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ message: "Invalid ID" });
+        }
+        const itemId = (id + 1).toString();
+
+        const report = reportItems.find((item) => item.reportId === itemId);
+
+        if (!report) {
+            return res.status(404).json({ message: "Report not found" });
+        }
+        const result = await qry.$queryRawUnsafe(report.query);
+        await qry.$disconnect();
+
         return res.status(200).json({ message: "success", data: result });
     } catch (error) {
-        // console.log(error);
+        console.error(error);
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
